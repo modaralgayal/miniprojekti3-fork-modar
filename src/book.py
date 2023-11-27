93f7c85c7b2c1e5b93125e2278cd64f9931aaee9
liisa <liisa-m@outlook.com>
2023-11-27 18:08:27 +0200

    user is able to create book/article/inproceeding type references

--- src/book.py
+++ src/book.py
@@ -17,34 +17,48 @@ def add_book(title, author, year, publisher, url):
     })
     db.session.commit()
 
-
 def get_books():
     sql = text(
         '''SELECT title, author, b_year, publisher, b_url FROM books''')
     result = db.session.execute(sql)
     books = result.fetchall()
-    #print(books)
     return books
 
-
-def add_article(title, author, year, publisher, url):
+def add_article(title, author, year, journal, url):
     sql = text(
-        '''INSERT INTO articles (title, author, a_year, publisher, a_url)
-        VALUES (:title, :author, :year, :publisher, :url)''')
+        '''INSERT INTO articles (title, author, a_year, journal, a_url)
+        VALUES (:title, :author, :year, :journal, :url)''')
     db.session.execute(sql, {
         "title": title,
         "author": author,
         "year": year,
-        "publisher": publisher,
+        "journal": journal,
         "url": url
     })
     db.session.commit()
 
-
 def get_articles():
     sql = text(
-        '''SELECT title, author, a_year, publisher, a_url FROM articles''')
+        '''SELECT title, author, a_year, journal, a_url FROM articles''')
     result = db.session.execute(sql)
     articles = result.fetchall()
-    #print(books)
     return articles
+
+def add_inproceeding(title, author, year, url):
+    sql = text(
+        '''INSERT INTO inproceedings (title, author, i_year, i_url)
+        VALUES (:title, :author, :year, :url)''')
+    db.session.execute(sql, {
+        "title": title,
+        "author": author,
+        "year": year,
+        "url": url
+    })
+    db.session.commit()
+
+def get_inproceedings():
+    sql = text(
+        '''SELECT title, author, i_year, i_url FROM inproceedings''')
+    result = db.session.execute(sql)
+    inproceedings = result.fetchall()
+    return inproceedings
