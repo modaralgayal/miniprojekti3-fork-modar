6120c3d7a826e96e9140b0e48d62e4151bb5a1ce
Yusuboy <Yuusufab@outlook.com>
2023-12-04 15:43:59 +0200

    yml

--- src/db.py
+++ src/db.py
@@ -1,9 +1,3 @@
 from flask_sqlalchemy import SQLAlchemy                                              
-from flask import Flask
 
-
-
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///test"
-db = SQLAlchemy(app)
+db = SQLAlchemy()
