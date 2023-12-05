96d0f5d511d9ff847e747473ba5ee8089bf78a35
Yusuboy <Yuusufab@outlook.com>
2023-12-05 15:33:51 +0200

    yml

--- src/app.py
+++ src/app.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 
 if getenv("production") == "test":
     db_address = "postgresql://test:test@localhost:5432"
-# elif getenv("production") == "test2":
-#     db_address = "postgresql://test:test@localhost:5432"
+# if getenv("production") == "test":
+#     db_address = "postgresql:///testi"
 else:
     db_address = getenv("DATABASE_URL")
 
